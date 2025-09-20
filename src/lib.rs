e4c5003865592f0d3568b3145ff280bb2a5dd929
winlogon <walker84837@gmail.com>
2025-09-20 03:36:42 +0200

    feat: add initial traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,75 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! # serverless-web
+
+#![warn(clippy::pedantic)]
+#![warn(missing_docs)]
+
+use http::StatusCode;
+use std::borrow::Cow;
+
+/// HTTP request
+pub trait Request {
+    /// ...
+    fn method(&self) -> &str;
+    /// ...
+    fn path(&self) -> &str;
+    /// ...
+    fn header(&self, name: &str) -> Option<Cow<'_, str>>;
+    /// ...
+    fn body(&self) -> &[u8];
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// ...
+pub trait Response {
+    /// ...
+    fn status(&self) -> StatusCode;
+    /// ...
+    fn header(&self, name: &str) -> Option<Cow<'_, str>>;
+    /// ...
+    fn body(&self) -> &[u8];
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// ...
+pub trait ResponseBuilder {
+    /// ...
+    type Resp: Response;
+
+    /// ...
+    fn new() -> Self;
+    /// ...
+    fn status(self, status: StatusCode) -> Self;
+    /// ...
+    fn header(self, name: &str, value: &str) -> Self;
+    /// ...
+    fn body(self, body: impl Into<Vec<u8>>) -> Self;
+    /// ...
+    fn build(self) -> Self::Resp;
+}
+
+/// ...
+pub trait Handler {
+    /// ...
+    type Req: Request;
+    /// ...
+    type Resp: Response;
+
+    /// ...
+    fn handle(&self, req: Self::Req) -> Self::Resp;
+}
+
+#[cfg(feature = "serde")]
+pub trait RequestExt: Request {
+    fn json_body<T: serde::de::DeserializeOwned>(&self) -> Result<T, serde_json::Error> {
+        serde_json::from_slice(self.body())
+    }
+}
+
+#[cfg(feature = "serde")]
+pub trait ResponseBuilderExt: ResponseBuilder {
+    fn json<T: serde::ser::Serialize>(self, value: &T) -> Result<Self::Resp, serde_json::Error> {
+        let bytes = serde_json::to_vec(value)?;
+        Ok(self
+            .header("content-type", "application/json")
+            .body(bytes)
+            .build())
     }
 }
